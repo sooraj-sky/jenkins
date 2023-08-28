ddc067fb6780084e76b056f63e512c99e7d44288
Sooraj KS <105536016+sooraj-sky@users.noreply.github.com>
2023-08-28 10:39:41 +0530

    Update tf.tf

--- tf.tf
+++ tf.tf
@@ -1,85 +1,113 @@
-resource "aws_sns_topic" "example_topic" {
-  name = "example-topic"
+resource "aws_s3_bucket" "target_bucket" {
+  bucket = "unique-target-bucket"  # Change this to your desired target bucket name
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-resource "aws_sqs_queue" "example_queue" {
-  name = "example-queue"
+resource "aws_s3_account_public_access_block" "public_access_block" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-resource "aws_sns_topic_subscription" "example_subscription" {
-  topic_arn = aws_sns_topic.example_topic.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.example_queue.arn
-}
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "unique-bucket1"  # Change this to your desired bucket name
 
-resource "aws_sns_platform_application" "example_platform_app" {
-  name     = "example-platform-app"
-  platform = "GCM"
-}
-
-resource "aws_sns_platform_endpoint" "example_platform_endpoint" {
-  platform_application = aws_sns_platform_application.example_platform_app.arn
-  token                = var.device_token
-}
-
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
-}
-
-resource "aws_iam_role" "example_iam_role" {
-  name = "example-iam-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "sns.amazonaws.com",
-        },
-      },
-    ],
-  })
-}
-
-resource "aws_iam_policy" "example_s3_policy" {
-  name = "example-s3-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ],
-        Effect   = "Allow",
-        Resource = "${aws_s3_bucket.example_bucket.arn}/*",
-      },
-      {
-        Action   = "s3:*",
-        Effect   = "Deny",
-        NotPrincipal = {
-          AWS = "*"
-        },
-        Principal = "*",
-        Resource = [
-          "${aws_s3_bucket.example_bucket.arn}/*"
-        ],
-        Condition = {
-          "Bool": {
-            "aws:SecureTransport": "false"
-          }
-        }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
       }
-    ],
-  })
+    }
+  }
+
+  # Enable access logging using the target_bucket
+  logging {
+    target_bucket = aws_s3_bucket.target_bucket.id
+    target_prefix = "bucket1-logs/"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "example_s3_policy_attachment" {
-  policy_arn = aws_iam_policy.example_s3_policy.arn
-  role       = aws_iam_role.example_iam_role.name
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "unique-bucket2"  # Change this to your desired bucket name
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  # Enable access logging using the target_bucket
+  logging {
+    target_bucket = aws_s3_bucket.target_bucket.id
+    target_prefix = "bucket2-logs/"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "bucket3" {
+  bucket = "unique-bucket3"  # Change this to your desired bucket name
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  # Enable access logging using the target_bucket
+  logging {
+    target_bucket = aws_s3_bucket.target_bucket.id
+    target_prefix = "bucket3-logs/"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_sqs_queue" "encrypted_queue" {
+  name            = "my-encrypted-queue"
+  kms_master_key_id = aws_kms_key.my_key.id
+}
+
+resource "aws_sns_topic" "my_topic" {
+  name = "my-topic"
+}
+
+resource "aws_sns_topic_subscription" "sqs_subscription" {
+  topic_arn = aws_sns_topic.my_topic.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.encrypted_queue.arn
+}
+
+resource "aws_sns_platform_application" "my_platform_app" {
+  name     = "my-platform-app"
+  platform = "APNS"  # Change this to the desired platform (e.g., "APNS" for Apple Push Notification Service)
+  platform_credential = "<APNS PRIVATE KEY>"
+  platform_principal  = "<APNS CERTIFICATE>"
+}
+
+resource "aws_sns_platform_endpoint" "my_platform_endpoint" {
+  platform_application_arn = aws_sns_platform_application.my_platform_app.arn
+  custom_user_data         = "my-custom-data"
+  token                    = "my-platform-endpoint-token"
+  kms_master_key_id        = aws_kms_key.my_key.id
+}
+
+resource "aws_kms_key" "my_key" {
+  description = "My KMS key"
 }
